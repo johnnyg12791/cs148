874449fc091321ac28d3323d17e540131b1fa444
John Gold <johnnyg12791@gmail.com>
2014-09-29 09:32:39 -0700

    cleaned it up a little

--- assignment1/main.cpp
+++ assignment1/main.cpp
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <math.h>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -15,7 +15,7 @@ int win_height = 512;
 int draw_mode=1;
 
 //Took away the arguments, I think it's easier to call the transforms first, then draw seperately
-void drawHalfArch(){
+void drawLeftHalfArch(){
     glBegin(GL_TRIANGLE_STRIP);
     
     //HELEN made all of the vertices 3-dimensional, again trying to solve the overlap problem by using z coordinates. No luck yet.
@@ -41,6 +41,40 @@ void drawHalfArch(){
     glEnd();
 }
 //as opposed to redrawing it, copy the previous image matrix
+void drawRightHalfArch(){
+    //Here is where I add the flipped arch
+    //There are 2 ways to do the translating, either before or after the scale
+    //from experimenting I found out it was 0.6f, or -0.6f if you do it after
+    //glTranslatef(0.6f, 0.0f, 0.0f);
+    glPushMatrix();
+    glScalef(-1.0f, 1.0f, 1.0f);
+    glTranslatef(-0.6f, 0.0f, 0.0f);
+    drawLeftHalfArch();
+    glPopMatrix();
+}
+
+
+
+//Draw a sun in the top left corner
+void addSun(){
+    glColor3f(1.f,1.f,0.f);
+    glBegin(GL_POLYGON);
+    //for (int i = 0; i < 8; i++) {
+    //    glVertex3d(sin(i/8.0*2*M_PI), cos(i/8.0*2*M_PI), 0.0f);
+    //}
+    
+    
+    glVertex3f(0.02f,0.88f,0.f);
+    glVertex3f(0.04f,0.94f,0.f);
+    glVertex3f(0.10f,0.98f,0.f);
+    glVertex3f(0.16f,0.94f,0.f);
+    glVertex3f(0.18f,0.88f,0.f);
+    glVertex3f(0.16f,0.84f,0.f);
+    glVertex3f(0.10f,0.79f,0.f);
+    glVertex3f(0.04f,0.84f,0.f);
+    
+    glEnd();
+}
 
 void display( void )
 {
@@ -53,27 +87,15 @@ void display( void )
           //First half of red arch
           glLoadIdentity();
           glPushMatrix();
-          glTranslatef(0.0f, 0.0f, 0.0f);
+          
+          //glTranslatef(0.0f, 0.0f, 0.0f);
           glColor3f(1.0f, 0.0f, 0.0f);
-          drawHalfArch();
-          
-          //Here is where I add the flipped arch
-          //There are 2 ways to do the translating, either before or after the scale
-          //from experimenting I found out it was 0.6f, or -0.6f if you do it after
-          //glTranslatef(0.6f, 0.0f, 0.0f);
+          drawLeftHalfArch();
           glColor3f(1.0f, 1.0f, 0.0f);
-          glPushMatrix();
-          glScalef(-1.0f, 1.0f, 1.0f);
-          glTranslatef(-0.6f, 0.0f, 0.0f);
-          drawHalfArch();
+          drawRightHalfArch();
           //now the flipped arch is added
-          
-          //when i pop this matrix, i remove the 2 things i did after i made the most recent push
-          //so, it will no longer scale and translate, and thus be back on the original place
-          //changed the color to white, so you can see that its back over the original spot
-          glPopMatrix();
-          glColor3d(1.0f, 1.0f, 1.0f);
-          drawHalfArch();
+
+          addSun();
           break;
           
       case 2:
@@ -82,12 +104,9 @@ void display( void )
           glPushMatrix();
           glTranslatef(0.0f, 0.0f, 0.0f);
           glColor3f(1.0f, 0.0f, 0.0f);
-          drawHalfArch();
+          drawLeftHalfArch();
           glColor3f(1.0f, 1.0f, 0.0f);
-          glPushMatrix();
-          glScalef(-1.0f, 1.0f, 1.0f);
-          glTranslatef(-0.6f, 0.0f, 0.0f);
-          drawHalfArch();
+          drawRightHalfArch();
           //now the flipped arch is added
           
           //when i pop this matrix, i remove the 2 things i did after i made the most recent push
@@ -100,20 +119,8 @@ void display( void )
           
           glTranslatef(0.15f,0.05f,0.5f);
           glScalef(0.85f, 0.85f, 0.f);
-          drawHalfArch();
-          //drawHalfArch(0.1f, 0.1f, 1.0f, 1.0f, 0.0f);
-          //glLineWidth(4.);
-          glLoadIdentity();
-          
-          //body
-          glPushMatrix(); //push identity
-          
-          //glBegin(GL_LINES);
-          //glColor3f(1.f,1.f,1.f);
-          //glVertex3f(0.0f,0.0f,0.f);
-          //glVertex3f(0.0f,-0.25f,0.f);
-          //glPushM
-          //Now lets say I want to reflect this...(as a copy)
+          drawLeftHalfArch();
+          drawRightHalfArch();
           glEnd();
           break;
 
